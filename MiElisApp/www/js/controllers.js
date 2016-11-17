9ececf52cb0925f19c1ce5c5792a2eb543935a3b
Schmocker K. <kasparsimon.schmocker@students.bfh.ch>
2016-11-17 11:35:46 +0100

    add GoogleChart

--- MiElisApp/www/js/controllers.js
+++ MiElisApp/www/js/controllers.js
@@ -31,81 +31,8 @@ angular.module('starter.controllers', [])
 
 })
 
-.controller('SugarCtrl', function($scope) {
-  google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
-
-var a = [
-  ['Zeit', 'Glucose mmol/L'],
-  [getDateStringI(7),  5.3],
-  [getDateStringI(6),  13.4],
-  [getDateStringI(5),  7.6],
-  [getDateStringI(4),  6.9],
-  [getDateStringI(3),  5.6],
-  [getDateStringI(2),  9.5],
-  [getDateStringI(1),  8.6]];
-
-function drawChart() {
-  var a_short = [
-    ['Zeit', 'Glucose mmol/L'],
-    a[a.length-7],
-    a[a.length-6],
-    a[a.length-5],
-    a[a.length-4],
-    a[a.length-3],
-    a[a.length-2],
-    a[a.length-1]
-  ];
-  var data = google.visualization.arrayToDataTable(a_short);
-
-
-  var options = {
-  fontSize: 20,
-  title: "Elisabeth's Diabetes",
-  curveType: 'none',
-  pointSize: 50,
-  legend: { position: 'bottom' }
-
-  };
-
-  var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
-
-  chart.draw(data, options);
-}
-
-function newValue(){
-  var r = (Math.random()*5 );
-  var last = a[a.length-1][1]-2;
-  var act = last+r;
-  document.getElementById("w_value").value = act.toFixed(1);
-  document.getElementById("b_add").disabled = false;
-}
-
-function addA(){
-var v = document.getElementById("w_value").value;
-document.getElementById("w_value").value ='';
-a.push([getDateString(), Number(v)]);
-drawChart();
-}
-
-function getDateString(){
-var d = new Date();
-var h = d.getHours();
-var min = d.getMinutes();
-var y = d.getFullYear();
-return h+":"+min+".";
-}
-function getDateStringI(i){
-var d = new Date();
-d.setHours(14);
-//d.setMinutes(00);
-d.setTime(d.getTime() - (i*28800000));
-var h = d.getHours();
-var min = d.getMinutes();
-var y = d.getFullYear();
-return h+':'+'00';
-}
-
+.controller('SugarCtrl', function($scope, Chart) {
+  Chart.drawChart();
 })
 
 .controller('PulseCtrl', function($scope) {
