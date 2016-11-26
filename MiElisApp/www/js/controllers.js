fded37363272d44099c460829adf29e4c579ca51
Schmocker K. <kasparsimon.schmocker@students.bfh.ch>
2016-11-26 13:36:01 +0100

    add area chart example in Pulse.html

--- MiElisApp/www/js/controllers.js
+++ MiElisApp/www/js/controllers.js
@@ -109,11 +109,126 @@ after verification, that it's a numeric value, the value is added to value array
 })
 
 .controller('SugarCtrl', function($scope, Chart) {
-//  Chart.drawChart();
+
 })
 
 .controller('PulseCtrl', function($scope) {
-
+  $scope.myChartObject = {
+    "type": "AreaChart",
+    "displayed": false,
+    "data": {
+      "cols": [
+        {
+          "id": "month",
+          "label": "Month",
+          "type": "string",
+          "p": {}
+        },
+        {
+          "id": "laptop-id",
+          "label": "Laptop",
+          "type": "number",
+          "p": {}
+        },
+        {
+          "id": "desktop-id",
+          "label": "Desktop",
+          "type": "number",
+          "p": {}
+        },
+        {
+          "id": "server-id",
+          "label": "Server",
+          "type": "number",
+          "p": {}
+        },
+        {
+          "id": "cost-id",
+          "label": "Shipping",
+          "type": "number"
+        }
+      ],
+      "rows": [
+        {
+          "c": [
+            {
+              "v": "January"
+            },
+            {
+              "v": 19,
+              "f": "42 items"
+            },
+            {
+              "v": 12,
+              "f": "Ony 12 items"
+            },
+            {
+              "v": 7,
+              "f": "7 servers"
+            },
+            {
+              "v": 4
+            }
+          ]
+        },
+        {
+          "c": [
+            {
+              "v": "February"
+            },
+            {
+              "v": 13
+            },
+            {
+              "v": 1,
+              "f": "1 unit (Out of stock this month)"
+            },
+            {
+              "v": 12
+            },
+            {
+              "v": 2
+            }
+          ]
+        },
+        {
+          "c": [
+            {
+              "v": "March"
+            },
+            {
+              "v": 24
+            },
+            {
+              "v": 5
+            },
+            {
+              "v": 11
+            },
+            {
+              "v": 6
+            }
+          ]
+        }
+      ]
+    },
+    "options": {
+      "title": "Sales per month",
+      "isStacked": "true",
+      "fill": 20,
+      "displayExactValues": true,
+      "vAxis": {
+        "title": "Sales unit",
+        "gridlines": {
+          "count": 10
+        }
+      },
+      "hAxis": {
+        "title": "Date"
+      }
+    },
+    "formatters": {}
+  }
 })
 
 .controller('MeCtrl', function($scope) {
